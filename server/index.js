a891b7f949511812d4b239e450523125d2ea961f
KOHE11 <55177338+KOHE11@users.noreply.github.com>
2024-02-24 16:39:19 +0900

    フロント → サーバー　をつなぎ込み完成

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+const cors = require("cors");
+
+app.use(cors());
+
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })

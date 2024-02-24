76c7ca6521eec29395901313f35a8a7e2c734b64
Yuto Matsuda <yuto.matsuda@mixi.co.jp>
2024-02-24 16:31:43 +0900

    feat(app): apply cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+const cors = require("cors");
+app.use(cors());
+
 app.get('/hello/', (req, res) => {
   res.send('Hello World!')
 })
